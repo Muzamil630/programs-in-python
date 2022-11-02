30ae03db0aa7fa75337399fcff30b6b904e6cc14
Muzamil630 <116160858+Muzamil630@users.noreply.github.com>
2022-11-02 15:30:04 +0530

    Add files via upload

---
+++ area_of_circle.py
@@ -0,0 +1,14 @@
+# """
+
+# """
+
+""" Another Method """
+"""
+import math
+r=float(input("Enter the Radius:"))
+Area=math.pi*r**2
+print("The Area of the Circle is:%.2f" %Area)
+"""
+
+# import math
+# print(math.pi)
---
+++ joining_strings.py
@@ -0,0 +1,7 @@
+s1,s2=input('Enter two Strings:').split(",")
+print(s1+s2)
+# print(s1,s2,sep="-")
+
+# sl=input("muzamil")
+# s2=input("gadi syed")
+# print(s1+s2)muza
